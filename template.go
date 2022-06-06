505217b946e55309cc64bcdf4d233a86100d2367
snowmerak <snowmerak@outlook.com>
2022-06-06 13:26:22 +0900

    remove search button

--- template.go
+++ template.go
@@ -4,7 +4,6 @@ var template = `
 <!DOCTYPE html>
 <html><head><link rel="stylesheet" href="./water.css"></head><body>
 <div style="display: flex; align-items: center; align-self: center; flex-direction: row; flex-wrap: wrap; justify-content: flex-end;">
-<button onclick="location.href='./search.html'">search</button>
 <button onclick="location.href='./tags.html'">tags</button>
 </div>%s</body></html>
 `
