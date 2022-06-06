1078864bf4c00b5438c3336cbdc07bd85154fec0
snowmerak <snowmerak@outlook.com>
2022-06-06 13:34:54 +0900

    add sub directory template

--- template.go
+++ template.go
@@ -1,9 +1,19 @@
 package main
 
-var template = `
+const template = `
 <!DOCTYPE html>
 <html><head><link rel="stylesheet" href="./water.css"></head><body>
 <div style="display: flex; align-items: center; align-self: center; flex-direction: row; flex-wrap: wrap; justify-content: flex-end;">
+<button onclick="location.href='./index.html'">index</button>
 <button onclick="location.href='./tags.html'">tags</button>
+</div>%s</body></html>
+`
+
+const subTemplate = `
+<!DOCTYPE html>
+<html><head><link rel="stylesheet" href="../water.css"></head><body>
+<div style="display: flex; align-items: center; align-self: center; flex-direction: row; flex-wrap: wrap; justify-content: flex-end;">
+<button onclick="location.href='../index.html'">index</button>
+<button onclick="location.href='../tags.html'">tags</button>
 </div>%s</body></html>
 `
